f3cbe4de1f23ab940ea231fdb490a482393c7576
追い剥ぎ <OIHAGI@oihagi-no-MacBook-Air.local>
2017-12-09 23:17:43 +0900

    no message

--- jquery.SPDiscussion.js
+++ jquery.SPDiscussion.js
@@ -2,13 +2,9 @@
 	RegisterSod("jquery.js", "//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
 	RegisterSod("jquery.SPServices.js","//cdnjs.cloudflare.com/ajax/libs/jquery.SPServices/2014.02/jquery.SPServices.min.js");
 
-	function AllPageScripting_OnPageLoad() {
-		LoadSodByKey("jquery.js",function(){
-			LoadSodByKey("jquery.SPServices.js",function(){
-				console.log("SPServices");
-			});
+	LoadSodByKey("jquery.js",function(){
+		LoadSodByKey("jquery.SPServices.js",function(){
+			console.log("SPServices");
 		});
-	};
-	// Call onload function
-	_spBodyOnLoadFunctionNames.push("AllPageScripting_OnPageLoad");
+	});
 })();
