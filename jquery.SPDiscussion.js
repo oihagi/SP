7668ca21c1c35dc8840de83401d036c81efb4b03
追い剥ぎ <OIHAGI@oihagi-no-MacBook-Air.local>
2017-12-09 23:09:22 +0900

    初期ロード

--- jquery.SPDiscussion.js
+++ jquery.SPDiscussion.js
@@ -1,7 +1,14 @@
 ;(function(){
-	console.log("外部")
 	RegisterSod("jquery.js", "//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
-	LoadSodByKey("jquery.js",function(){
+	RegisterSod("jquery.SPServices.js","//cdnjs.cloudflare.com/ajax/libs/jquery.SPServices/2014.02/jquery.SPServices.min.js");
 
-	});
+	function AllPageScripting_OnPageLoad() {
+		LoadSodByKey("jquery.js",function(){
+			LoadSodByKey("jquery.SPServices.js",function(){
+				console.log("SPServices");
+			});
+		});
+	};
+	// Call onload function
+	_spBodyOnLoadFunctionNames.push("AllPageScripting_OnPageLoad");
 })();
