c133e0cdd5c7d1346a96a1af28417e45921c987b
追い剥ぎ <OIHAGI@oihagi-no-MacBook-Air.local>
2017-12-10 22:01:36 +0900

    RESTのベースを追加

--- jquery.SPDiscussion.js
+++ jquery.SPDiscussion.js
@@ -4,7 +4,65 @@
 
 	LoadSodByKey("jquery.js",function(){
 		LoadSodByKey("jquery.SPServices.js",function(){
-			console.log("SPServices");
+			init();
 		});
 	});
+	function init(){
+		try{
+			_spPageContextInfo = $.extend({}, _spPageContextInfo);
+		}catch(err){
+			console.log(err);
+			return;
+		};
+		console.log(_spPageContextInfo);
+		rest({"path":"/_api/contextinfo","type":"POST"}).done(function(data){
+			console.log(data);
+		});
+		rest({"path":"/_api/lists"}).done(function(data){
+			console.log(data);
+		});
+		if(!_spPageContextInfo.listId){
+			return;
+		};
+		rest({"listName":_spPageContextInfo.listId}).done(function(data){
+			console.log(data);
+		});
+		rest({"listName":_spPageContextInfo.listId,"path":"/items","query":"?$select=*"}).done(function(data){
+			console.log(data);
+		});
+	};
+	//ajax通信をし、返ってきた値をリストに追加する
+	function rest(args){
+		args = args || {};
+		//完了を知らせるためにDeferredオブジェクトを生成しそれを返す
+		var deferred_ = new $.Deferred();
+		var options_ = {
+			"type": args.type || "GET"
+			,"url": _spPageContextInfo.webAbsoluteUrl
+			,"headers": {
+				"Access-Control-Allow-Origin": "*"
+				,"accept": "application/json;odata=verbose"
+				,"content-type":"application/json;odata=verbose"
+			}
+		};
+		if(args.listName){
+			options_.url += "/_api/web/lists(guid'"+args.listName.replace("{","").replace("}","")+"')";
+		};
+		if(args.path){
+			args.path = args.path.indexOf("/")!=0 ? "/"+args.path : args.path;
+			options_.url += args.path;
+		};
+		if(args.query){
+			args.query = args.query.indexOf("?")!=0 ? "?"+args.query : args.query;
+			options_.url += args.query;
+		};
+		$.ajax(options_)
+			.done(function(data){
+				//ajax処理を終了したことをDeferredオブジェクトに通知
+				deferred_.resolve(data);
+			})
+			.fail(function(){});
+		//完了を知らせるためにDeferredオブジェクトを生成しそれを返す
+		return deferred_ .promise(this);
+	};
 })();
